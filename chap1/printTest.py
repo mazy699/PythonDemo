723c135b2e008670dd280ff6f760c1015ae5a836
Mazy <mazhongyuan699@outlook.com>
2022-01-14 20:55:56 +0800

    使用print进行复杂的输出

--- chap1/printTest.py
+++ chap1/printTest.py
@@ -11,3 +11,11 @@ print(a, b, "hello python")  # 输出多个值
 print('b')  # 输出字符
 print(chr(98))
 print(ord('b'))
+print('hello', end='-->')  # 输出不换行
+print('python')
+print(1314)  # 直接输出整数
+print(3.14)  # 直接输出小数
+print(1, 2, 3, 4)  # 使用逗号连接要输出的数字，中间使用空格连接
+print(192, 168, 1, 1, sep='.')  # 使用间隔符.进行连接，数值之间通过.进行分隔
+# print('hello python'+2022)#TypeError: can only concatenate str (not "int") to str
+print('hello python' + '2022')
