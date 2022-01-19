0e39d36b2433fbc06fc03565129dfd0697d9ea5f
Mazy <mazhongyuan699@outlook.com>
2022-01-19 16:45:14 +0800

    基本数据类型——字符串类型

--- chap2/basic_data_type.py
+++ chap2/basic_data_type.py
@@ -46,3 +46,41 @@ print(round(0.1 + 0.2, 1))  # 保留1位小数
 num7 = 12 + 34j
 print(num7.real)
 print(num7.imag)
+
+"""
+字符串类型
+    连续的字符序列，可以表示计算机能识别的一切字符
+    不可变数据类型，即不可变字符序列
+    单行字符串使用'......'或"......"
+"""
+# 多行字符串使用'''......'''或"""......"""
+
+str1 = 'abc'
+print(str1)
+
+str2 = '''abc
+123
+def'''
+print(str2)
+
+# 转义字符\n改行
+print('aaa\nbbb')
+
+# 字符串索引
+str3 = 'helloworld'
+print(str3[0], str3[-10])  # 正向递增序号从0开始，反向递减序号从-1开始
+
+# 字符串切片
+print(str3[5:9])  # 正向递增序列，不包括9
+print(str3[5:])  # 正向递增序列，从5开始到末尾
+print(str3[-5:-1])  # 反向递减序列，不包括-1
+print(str3[-5:])  # 反向递减序列，从-5开始到末尾
+
+# 字符串类型的操作
+# x+y 连接两个字符串
+# x*n 复制n次字符串
+# x in s 如果x是s的字串，结果为True，否则为False
+print('ddd' + 'eee')
+print('fff' * 3)
+print('ab' in 'abcde')
+print('ab' in 'cd')
