40c819a4b4bfdf114622db562f3c6da5b4eed9ec
mazy <mazhongyuan699@outlook.com>
2023-03-16 10:06:00 +0800

    c03-03添加demo

--- Python-Core-50-Courses/c03/c03-03.py
+++ Python-Core-50-Courses/c03/c03-03.py
@@ -8,14 +8,18 @@ a = 100
 b = 12.345
 c = 'hello, world'
 d = True
+d1 = False
+e = ""
 # 整数转成浮点数
 print(float(a))    # 100.0
 # 浮点型转成字符串 (输出字符串时不会看到引号哟)
 print(str(b))      # 12.345
 # 字符串转成布尔型 (有内容的字符串都会变成True)
+print(bool(e))     # False
 print(bool(c))     # True
 # 布尔型转成整数 (True会转成1，False会转成0)
 print(int(d))      # 1
+print(int(d1))      # 0
 # 将整数变成对应的字符 (97刚好对应字符表中的字母a)
 print(chr(97))     # a
 # 将字符转成整数 (Python中字符和字符串表示法相同)
