30043088dee6c686466dd26e0c71f16b265841d9
mazy <mazhongyuan699@outlook.com>
2023-03-16 21:39:39 +0800

    Python-Core-50-Courses学习course12

---
+++ Python-Core-50-Courses/c12/c12-01.py
@@ -0,0 +1,33 @@
+xinhua = {
+    '麓': '山脚下',
+    '路': '道，往来通行的地方；方面，地区：南～货，外～货；种类：他俩是一～人',
+    '蕗': '甘草的别名',
+    '潞': '潞水，水名，即今山西省的浊漳河；潞江，水名，即云南省的怒江'
+}
+print(xinhua)
+person = {
+    'name': '王大锤', 'age': 55, 'weight': 60, 'office': '科华北路62号',
+    'home': '中同仁路8号', 'tel': '13122334455', 'econtact': '13800998877'
+}
+print(person)
+
+print('-----------------------------')
+# dict函数(构造器)中的每一组参数就是字典中的一组键值对
+person = dict(name='王大锤', age=55, weight=60, home='中同仁路8号')
+print(person)    # {'name': '王大锤', 'age': 55, 'weight': 60, 'home': '中同仁路8号'}
+
+# 可以通过Python内置函数zip压缩两个序列并创建字典
+items1 = dict(zip('ABCDE', '12345'))
+print(items1)    # {'A': '1', 'B': '2', 'C': '3', 'D': '4', 'E': '5'}
+items2 = dict(zip('ABCDE', range(1, 10)))
+print(items2)    # {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
+
+# 用字典生成式语法创建字典
+items3 = {x: x ** 3 for x in range(1, 6)}
+print(items3)     # {1: 1, 2: 8, 3: 27, 4: 64, 5: 125}
+
+print('-----------------------------')
+person = {'name': '王大锤', 'age': 55, 'weight': 60, 'office': '科华北路62号'}
+print(len(person))    # 4
+for key in person:
+    print(key)
---
+++ Python-Core-50-Courses/c12/c12-04.py
@@ -0,0 +1,20 @@
+sentence = input('请输入一段话: ')
+counter = {}
+for ch in sentence:
+    if 'A' <= ch <= 'Z' or 'a' <= ch <= 'z':
+        counter[ch] = counter.get(ch, 0) + 1
+for key, value in counter.items():
+    print(f'字母{key}出现了{value}次.')
+    
+print('----------------------------')
+stocks = {
+    'AAPL': 191.88,
+    'GOOG': 1186.96,
+    'IBM': 149.24,
+    'ORCL': 48.44,
+    'ACN': 166.89,
+    'FB': 208.09,
+    'SYMC': 21.29
+}
+stocks2 = {key: value for key, value in stocks.items() if value > 100}
+print(stocks2)
